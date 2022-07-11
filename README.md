8b5000089523fefddf65b7b0f28add864f23e693
Luke Wood <lukewoodcs@gmail.com>
2022-07-11 14:15:03 -0700

    updates to RetinaNet

---
+++ README.md
@@ -0,0 +1,3 @@
+# KerasCV RetinaNet
+
+KerasCV RetinaNet is a demonstration of what a RetinaNet model would look like written in the style of KerasCV.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 VERSION = "0.0.0"
 
 setup(
-    name="retina-net",
+    name="retina_net",
     packages=find_packages(exclude=("*_test.py",)),
     version=VERSION,
     description="KerasCV RetinaNet model",
