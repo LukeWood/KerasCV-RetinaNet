de37a315225fd0fecaaa57b73cfdd68d6c217187
Luke Wood <lukewoodcs@gmail.com>
2022-07-13 08:59:12 -0700

    update optimizer

--- cli/train.py
+++ cli/train.py
@@ -83,20 +83,32 @@ metrics = [
     ),
 ]
 
+# train_ds is batched as a (images, bounding_boxes) tuple
+# bounding_boxes are ragged
 train_ds = load_pascal_voc(bounding_box_format="xywh", split="train", batch_size=2)
+val_ds = load_pascal_voc(bounding_box_format="xywh", split="train", batch_size=2)
+
+learning_rates = [2.5e-06, 0.000625, 0.00125, 0.0025, 0.00025, 2.5e-05]
+learning_rate_boundaries = [125, 250, 500, 240000, 360000]
+learning_rate_fn = tf.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries=learning_rate_boundaries, values=learning_rates
+)
+
+optimizer = tf.optimizers.SGD(
+    learning_rate=learning_rate_fn, momentum=0.9, global_clipnorm=10.0
+)
 
 # No rescaling
 model = retina_net.RetinaNet(
     num_classes=20, bounding_box_format="xywh", include_rescaling=False
 )
 model.compile(
-    optimizer="adam",
-    loss=retina_net.FocalLoss(num_classes=20),
-    metrics=metrics
+    optimizer=optimizer, loss=retina_net.FocalLoss(num_classes=20), metrics=metrics
 )
 
+
 def unpackage_dict(inputs):
-    return inputs["images"]/255.0, inputs["bounding_boxes"]
+    return inputs["images"] / 255.0, inputs["bounding_boxes"]
 
 
 train_ds = train_ds.map(unpackage_dict, num_parallel_calls=tf.data.AUTOTUNE)
@@ -106,4 +118,4 @@ callbacks = [
     WandbCallback(),
 ]
 
-model.fit(train_ds, epochs=100, callbacks=callbacks)
+model.fit(train_ds, validation_data=val_ds, epochs=100, callbacks=callbacks)
